fa0956eb27cf357681f8d0ab9ee9adbda48d2a83
darya-frsd <90180904+darya-frsd@users.noreply.github.com>
2022-05-28 14:15:32 +0300

    Update lab04-figure.cpp

--- lab04-figure.cpp
+++ lab04-figure.cpp
@@ -1,7 +1,12 @@
-﻿#include "lab04-figure.h"
+#include "lab04-figure.h"
 #include "lab04-parallelogram.h"
+#include "lab04-triangle.h"
 
 int main()
 {
+	parallelogram a;
+	a.initialization();
+	a.draw();
+	a.square();
 	return 0;
 }
